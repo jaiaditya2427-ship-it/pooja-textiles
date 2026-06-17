9bc5cc7fa9ec64301db75db39a29dabbe375e7b3
jaiaditya2427-ship-it <jaiaditya2427@gmail.com>
2026-06-17 10:51:05 +0530

    Fix PWA service worker

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,41 @@
-const CACHE_NAME = "fashion-tryon-v1";
+const CACHE_NAME = "pooja-textiles-v2";
 
 const urlsToCache = [
-  "/",
-  "/index.html"
+  "/fashiontryon/poojatextiles/",
+  "/fashiontryon/poojatextiles/index.html",
+  "/fashiontryon/poojatextiles/manifest.json",
+  "/fashiontryon/poojatextiles/icon-192.png",
+  "/fashiontryon/poojatextiles/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
     })
   );
 });
