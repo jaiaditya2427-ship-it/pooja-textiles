3e1fc60dd827ebbf151a6a12d762a78f947265d3
jaiaditya2427-ship-it <jaiaditya2427@gmail.com>
2026-08-20 00:57:32 +0530

    Fix main.jsx conflict

--- main.jsx
+++ main.jsx
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -16,55 +13,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
-// PWA service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> 3721339911efb63a0d3e218bc2bca258a9d1d79a
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
-// PWA service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> origin/main
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
-// PWA service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> 3721339911efb63a0d3e218bc2bca258a9d1d79a
 );
